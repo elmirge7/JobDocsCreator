a992514284879c39992a1971ca776ef5fea3a6b4
elmirge7 <elmirge7@gmail.com>
2024-03-18 13:55:07 +0100

    redesign of homepage

--- JobDocsCreator/mainwindow.cpp
+++ JobDocsCreator/mainwindow.cpp
@@ -8,22 +8,35 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->centralwidget->setStyleSheet("background-color: #FEFEFE");
+    // Définition d'un arrière-plan plus sombre pour le centralwidget
+    ui->centralwidget->setStyleSheet("background-color: #10002b");
+
+    // Centrage horizontal du bouton dans le layout vertical
     ui->verticalLayout->setAlignment(ui->pushButton, Qt::AlignHCenter);
+
+    // Définition d'un style plus sombre pour le bouton "Créer un CV"
     ui->pushButton->setMinimumWidth(200);
-    ui->pushButton->setStyleSheet("#pushButton {"
-        "color: #111111;"
-        "background-color: #CCD5AE;"
-        "border: 0;"
-        "transition-duration: 1s"
+    ui->pushButton->setStyleSheet(
+        "#pushButton {"
+        "   color: #FEFEFE;"
+        "   background-color: #5a189a;"
+        "   border: 0px solid transparent;"
+        "   border-radius: 25px;"
         "}"
         "#pushButton:hover {"
-        "color: #111111;"
-        "background-color: #E9EDC9;"
-        "border: 0;"
-        "transition-duration: 1s"
+        "   background-color: #7b2cbf;"
         "}"
-                                  );
+        );
+
+    // Changement de la couleur du titre
+    ui->appName->setStyleSheet("color: #FEFEFE;");
+
+    // Changement de la couleur du sous-titre
+    ui->slogan->setStyleSheet("color: #FEFEFE;");
+
+
+    setWindowTitle("JobDocs : Créateur de CV");
+
 }
 
 MainWindow::~MainWindow()
