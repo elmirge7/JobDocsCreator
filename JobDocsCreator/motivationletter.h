ba336fe8a1a1341289237f90e02930ca89c75333
NoamJG <113251704+Na-m0@users.noreply.github.com>
2024-03-24 10:05:47 +0100

    Motivation letter generator functionnality added

---
+++ JobDocsCreator/motivationletter.h
@@ -0,0 +1,35 @@
+#ifndef MOTIVATIONLETTER_H
+#define MOTIVATIONLETTER_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class MotivationLetter;
+}
+
+class MotivationLetter : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    explicit MotivationLetter(QWidget *parent = nullptr);
+    ~MotivationLetter();
+
+private slots:
+    void saveLetter(); // Déclaration de la fonction saveLetter()
+    void importText(); // Déclaration de la fonction importText()
+    void setUnderline();
+    void setBold();
+    void setItalic();
+    void alignLeft();
+    void alignCenter();
+    void alignRight();
+    void alignJustify();
+    void exportToPDF();
+
+
+private:
+    Ui::MotivationLetter *ui;
+};
+
+#endif // MOTIVATIONLETTER_H
